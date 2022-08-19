3060223292fad2b04f7b236249c3f1abb3cca2ff
23nith <zenithmanuel@gmail.com>
2022-08-19 21:32:40 +0800

    Add Current_User request rspec tests

--- spec/requests/current_user_spec.rb
+++ spec/requests/current_user_spec.rb
@@ -1,10 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe "CurrentUsers", type: :request do
-  describe "GET /index" do
+  describe "GET /current_user" do
+    before do
+      @user = create(:user)
+      sign_in @user
+    end
     it "returns http success" do
-      get "/current_user/index"
+      get "/current_user"
       expect(response).to have_http_status(:success)
+    end
+
+    it "Returns the json with id equal to the id of current user logged in" do
+      get "/current_user"
+      current_user = JSON.parse(response.body)
+      expect(current_user["id"]).to match(@user.id)
     end
   end
 
