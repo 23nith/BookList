abf5f74a6fcd9248b2ddd23ed416d047cb22492d
23nith <zenithmanuel@gmail.com>
2022-08-19 21:05:09 +0800

    Generate ListItems controller

---
+++ app/controllers/list_items_controller.rb
@@ -0,0 +1,19 @@
+class ListItemsController < ApplicationController
+  def reading_list
+  end
+
+  def finished_books
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
