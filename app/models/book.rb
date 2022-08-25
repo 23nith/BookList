9e559ebddc22b8e7836c04a64bc7ee7da1103f93
23nith <zenithmanuel@gmail.com>
2022-08-25 22:43:31 +0800

    Add validation to book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,11 @@
 class Book < ApplicationRecord
   has_many :list_items
   has_many :users, through: :list_items
+
+  validates :title, presence: true
+  validates :author, presence: true
+  validates :cover_image_url, presence: true
+  validates :page_count, presence: true, numericality: true
+  validates :publisher, presence: true
+  validates :synopsis, presence: true
 end
