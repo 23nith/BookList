e55a71b76d3fa5efc85269446ec33424eb55fe22
23nith <zenithmanuel@gmail.com>
2022-08-25 22:51:01 +0800

    Add validation to list_item model

--- app/models/list_item.rb
+++ app/models/list_item.rb
@@ -3,4 +3,8 @@
 class ListItem < ApplicationRecord
   belongs_to :book
   belongs_to :user
+
+  validates :book_id, presence: true
+  validates :user_id, presence: true
+  validates :rating, presence: true, numericality: { in: 1..5 }
 end
