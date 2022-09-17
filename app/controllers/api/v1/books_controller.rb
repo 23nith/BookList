2863a0fd828bfe443629c25728b42810b00121b8
23nith <user.name>
2022-09-17 17:35:38 +0800

    Fix books endpoint

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::BooksController < ApplicationController
   before_action :set_book, only: %i[show]
 
   def index
-    @list_array = ListItem.where(id: current_user.id).select(:book_id)
+    @list_array = ListItem.where(user_id: current_user.id).select(:book_id)
     @books = Book.where.not(id: @list_array)
 
     render json: @books
